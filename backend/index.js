214dc235517f9a925f431c00a3bffd20bdc5cd90
Akshay karada <akshaykarada72.o@gmail.com>
2025-07-03 02:07:01 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 const dotenv = require('dotenv');
-const contactRoutes = require('./routes/contact');
+const contactRoutes = require('./routes/router');
 
 
 // Load environment variables
